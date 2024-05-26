0508756814bfc517f85cad5dfea218f84be1998e
Ren <152279955+ChristianJude23@users.noreply.github.com>
2024-05-26 12:50:02 +0800

    Add files via upload

--- KitchenChaos/ProgressBarUI.cs
+++ KitchenChaos/ProgressBarUI.cs
@@ -5,18 +5,23 @@ using UnityEngine.UI;
 
 public class ProgressBarUI : MonoBehaviour {
 
-    [SerializeField] private CuttingCounter cuttingCounter;
+    [SerializeField] private GameObject hasProgressGameObject;
     [SerializeField] private Image barImage;
 
+    private IHasProgress hasProgress;
 
     private void Start() {
-        cuttingCounter.OnProgressChanged += CuttingCounter_OnProgressChanged;
+        hasProgress = hasProgressGameObject.GetComponent<IHasProgress>();
+        if (hasProgress == null ) {
+            Debug.LogError("Game Object " + hasProgressGameObject + " does not have a component that impelments IHasProgress!");
+        }
+        hasProgress.OnProgressChanged += HasProgress_OnProgressChanged;
         barImage.fillAmount = 0;
 
         Hide();
     }
 
-    private void CuttingCounter_OnProgressChanged(object sender, CuttingCounter.OnProgressChangedEventArgs e) {
+    private void HasProgress_OnProgressChanged(object sender, IHasProgress.OnProgressChangedEventArgs e) {
         barImage.fillAmount = e.progressNormalized;
 
         if (e.progressNormalized == 0f || e.progressNormalized == 1f) {
